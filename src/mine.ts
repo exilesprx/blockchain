527c05fd4df4f740768ed101ae86397102f478d8
Exiles.prx <campbell.andrew86@yahoo.com>
2022-01-08 13:53:38 -0500

    --WIP--

---
+++ src/mine.ts
@@ -0,0 +1,16 @@
+import { consumer } from "./stream/consumer";
+
+const run = async () => {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'transaction-added', fromBeginning: false })
+    await consumer.run({
+    
+    eachMessage: async ({ topic, partition, message }) => {
+        const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+        console.log(`- ${prefix} ${message.key}#${message.value}`)
+    },
+    });
+    await consumer.disconnect();
+}
+
+run();
