eb3ee86da6676206a4fad7214520ca4cbee4c410
Exilesprx <campbell.andrew86@yahoo.com>
2022-02-27 19:56:09 -0500

    Consumer group
    Updating to be controlled via .env settings

--- src/stream/consumer.ts
+++ src/stream/consumer.ts
@@ -1,3 +1,3 @@
 import { kafka } from './kafka.js';
 
-export const consumer = kafka.consumer({ groupId: 'test-group' })
+export const consumer = kafka.consumer({ groupId: `${process.env.KAFKA_GROUP_ID}` });
