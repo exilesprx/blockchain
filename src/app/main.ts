f41ba58d9f2b531c00ca83c2bfefbf482f280061
Exilesprx <campbell.andrew86@yahoo.com>
2022-03-23 21:14:35 -0400

    Eslint
    Start to eslint integration

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,94 +1,89 @@
-import EventEmitter from "events";
-import express, { Express } from "express";
-import Blockchain from "../domain/chain/blockchain";
-import Events from "../domain/events/emitter";
-import { logger } from "../domain/logs/logger";
-import { default as kafka } from "../domain/stream/kafka";
-import Producer from "../domain/stream/producer";
-import BlockConsumer from "../domain/stream/block-consumer";
-import Amount from "../domain/wallet/specifications/amount";
-import Receiver from "../domain/wallet/specifications/receiver";
-import SameWallet from "../domain/wallet/specifications/same-wallet";
-import Sender from "../domain/wallet/specifications/sender";
-import TransactionPool from "../domain/wallet/transaction-pool";
-import Transaction, { default as TransactionRoute } from "./routes/transaction";
-import Database from "../database";
-import Bank from "../domain/bank";
-import Link from "../domain/chain/specifications/link";
+/* eslint import/no-unresolved: 2 */
+import EventEmitter from 'events';
+import express, { Express } from 'express';
+import Blockchain from '../domain/chain/blockchain';
+import Events from '../domain/events/emitter';
+import { logger } from '../domain/logs/logger';
+import { default as kafka } from '../domain/stream/kafka';
+import Producer from '../domain/stream/producer';
+import BlockConsumer from '../domain/stream/block-consumer';
+import Amount from '../domain/wallet/specifications/amount';
+import Receiver from '../domain/wallet/specifications/receiver';
+import SameWallet from '../domain/wallet/specifications/same-wallet';
+import Sender from '../domain/wallet/specifications/sender';
+import TransactionPool from '../domain/wallet/transaction-pool';
+import Transaction, { default as TransactionRoute } from './routes/transaction';
+import Database from '../database';
+import Bank from '../domain/bank';
+import Link from '../domain/chain/specifications/link';
 
-export default class Application
-{
-    private app: Express;
+export default class Application {
+  private app: Express;
 
-    private emitter: EventEmitter;
+  private emitter: EventEmitter;
 
-    private events: Events;
+  private events: Events;
 
-    private chain: Blockchain;
+  private chain: Blockchain;
 
-    private pool: TransactionPool;
+  private pool: TransactionPool;
 
-    private bank: Bank;
+  private bank: Bank;
 
-    private database: Database;
+  private database: Database;
 
-    private producer: Producer;
+  private producer: Producer;
 
-    private consumer: BlockConsumer;
+  private consumer: BlockConsumer;
 
-    constructor()
-    {
-        this.app = express();
+  constructor() {
+    this.app = express();
 
-        this.database = new Database(String(process.env.DB_HOST), Number(process.env.DB_PORT));
+    this.database = new Database(String(process.env.DB_HOST), Number(process.env.DB_PORT));
 
-        this.emitter = new EventEmitter();
+    this.emitter = new EventEmitter();
 
-        this.producer = new Producer(kafka);
+    this.producer = new Producer(kafka);
 
-        this.events = Events.register(this.emitter, this.producer, logger);
+    this.events = Events.register(this.emitter, this.producer, logger);
 
-        this.chain = new Blockchain();
+    this.chain = new Blockchain();
 
-        this.pool = new TransactionPool();
+    this.pool = new TransactionPool();
 
-        this.bank = new Bank(this.pool, this.chain, this.events);
+    this.bank = new Bank(this.pool, this.chain, this.events);
 
-        this.consumer = new BlockConsumer(this.bank, this.database, kafka);
+    this.consumer = new BlockConsumer(this.bank, this.database, kafka);
+  }
 
-    }
+  public init() {
+    this.app.use(express.json());
 
-    public init()
-    {
-        this.app.use(express.json());
+    this.pool.addSpecification(new Amount())
+      .addSpecification(new Receiver())
+      .addSpecification(new Sender())
+      .addSpecification(new SameWallet());
 
-        this.pool.addSpecification(new Amount())
-            .addSpecification(new Receiver())
-            .addSpecification(new Sender())
-            .addSpecification(new SameWallet());
+    this.chain.addSpecification(new Link());
+  }
 
-        this.chain.addSpecification(new Link());
-    }
+  public async boot() {
+    this.database.connect();
 
-    public async boot()
-    {
-        this.database.connect();
+    this.producer.connect();
 
-        this.producer.connect();
+    // TODO: restore from eventstore, we only need to worry about block heres, the auditor will handle transactions
 
-        // TODO: restore from eventstore, we only need to worry about block heres, the auditor will handle transactions
+    this.consumer.connect();
 
-        this.consumer.connect();
+    this.app.listen(process.env.APP_PORT, () => {
+      logger.info(`App listening on port ${process.env.APP_PORT}`);
+    });
+  }
 
-        this.app.listen(process.env.APP_PORT, () => {
-            logger.info(`App listening on port ${process.env.APP_PORT}`);
-        });
-    }
+  public registerRoutes() {
+    const transactionRoute = new Transaction(this.database, this.bank);
 
-    public registerRoutes()
-    {
-        const transactionRoute = new Transaction(this.database, this.bank);
-        
-        this.app.post(TransactionRoute.getName(), transactionRoute.getAction.bind(transactionRoute));
-    }
+    this.app.post(TransactionRoute.getName(), transactionRoute.getAction.bind(transactionRoute));
+  }
 }
