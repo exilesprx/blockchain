b4741a71e1a509a4dba712d2d5145adeb0620d8a
Exilesprx <campbell.andrew86@yahoo.com>
2022-04-20 00:00:37 -0400

    Adding tests for transaction pool

--- src/domain/wallet/transaction-pool.ts
+++ src/domain/wallet/transaction-pool.ts
@@ -4,17 +4,27 @@ import Transaction from './transaction';
 export default class TransactionPool {
   private specifications: Specification[];
 
+  private transactions: Transaction[];
+
   constructor() {
     this.specifications = [];
+
+    this.transactions = [];
   }
 
   public fill(transaction: Transaction) : void {
     this.specifications.forEach((spec: Specification) => {
       spec.isSatisfiedBy(transaction);
     });
+
+    this.transactions.push(transaction);
   }
 
   public addSpecification(...specification: Specification[]) : void {
     this.specifications.push(...specification);
+  }
+
+  public isEmpty() : boolean {
+    return this.transactions.length === 0;
   }
 }
--- tests/transaction-pool.test.ts
+++ tests/transaction-pool.test.ts
@@ -1,13 +1,50 @@
+import Amount from '../src/domain/wallet/specifications/amount';
+import Transaction from '../src/domain/wallet/transaction';
+import TransactionPool from '../src/domain/wallet/transaction-pool';
+
 describe('Transaction pool', () => {
   test('it expects the pool to be empty when initialized', () => {
-    //
+    const pool = new TransactionPool();
+
+    expect(pool.isEmpty()).toBeTruthy();
   });
 
-  test('it expects the pool to have 1 transaction', () => {
-    //
+  test('it expects a valid transaction is added to the pool', () => {
+    const pool = new TransactionPool();
+
+    const transaction = new Transaction('to', 'from', 2);
+
+    pool.addSpecification(new Amount());
+
+    pool.fill(transaction);
+
+    expect(pool.isEmpty()).toBeFalsy();
   });
 
-  test('it expects the pool to drain', () => {
-    //
+  test('it expects an invalid transaction is not added to the pool', () => {
+    const pool = new TransactionPool();
+
+    const transaction = new Transaction('to', 'from', 0);
+
+    pool.addSpecification(new Amount());
+
+    expect(() => pool.fill(transaction)).toThrow();
+  });
+
+  test('it expects the ability to add multiple specifications at once', () => {
+    const pool = new TransactionPool();
+
+    const transaction = new Transaction('to', 'from', 5);
+
+    const amountSpec = new Amount();
+
+    const spy = jest.spyOn(amountSpec, 'isSatisfiedBy')
+      .mockReturnValue(true);
+
+    pool.addSpecification(amountSpec, amountSpec, amountSpec);
+
+    pool.fill(transaction);
+
+    expect(spy).toBeCalledTimes(3);
   });
 });
