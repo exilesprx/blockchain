c3960a9728895a8535a75e37ef2fb3d3abc2b65b
Exilesprx <campbell.andrew86@yahoo.com>
2022-05-19 22:25:57 -0400

    Rename app to bank

--- run-build.sh
+++ run-build.sh
@@ -5,8 +5,8 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo -e "\033[35m Updating app image blockchain:${BRANCH} \033[0m"
 docker build -f .docker/app/Dockerfile -t blockchain:${BRANCH} .
 
-echo -e "\033[33m Running build:app... \033[0m"
-docker run -it blockchain:${BRANCH} npm run build:app
+echo -e "\033[33m Running build:bank... \033[0m"
+docker run -it blockchain:${BRANCH} npm run build:bank
 
 echo -e "\033[33m Running build:miner... \033[0m"
 docker run -it blockchain:${BRANCH} npm run build:miner
