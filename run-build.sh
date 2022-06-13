96f97e0d485cf5aef54d21be1bb6dec4d8d22329
Exilesprx <campbell.andrew86@yahoo.com>
2022-06-12 21:02:43 -0400

    Docker buildkit
    Adding buildkit for squence of copy producing a null effect and causing
    a failed image layer.

--- run-build.sh
+++ run-build.sh
@@ -3,12 +3,12 @@
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 echo -e "\033[35m Updating app image blockchain:${BRANCH} \033[0m"
-docker build -f .docker/app/Dockerfile -t blockchain:${BRANCH} .
+DOCKER_BUILDKIT=1 docker build -f .docker/app/Dockerfile -t blockchain:${BRANCH} .
 
 echo -e "\033[33m Running build:bank... \033[0m"
-docker run -it blockchain:${BRANCH} npm run build:bank
+DOCKER_BUILDKIT=1 docker run -it blockchain:${BRANCH} npm run build:bank
 
 echo -e "\033[33m Running build:miner... \033[0m"
-docker run -it blockchain:${BRANCH} npm run build:miner
+DOCKER_BUILDKIT=1 docker run -it blockchain:${BRANCH} npm run build:miner
 
 echo -e "\033[32m Done! \033[0m"
--- run-test.sh
+++ run-test.sh
@@ -3,9 +3,9 @@
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 echo -e "\033[35m Updating test image blockchain-tests:${BRANCH} \033[0m"
-docker build -f .docker/tests/Dockerfile -t blockchain-tests:${BRANCH} .
+DOCKER_BUILDKIT=1 docker build -f .docker/tests/Dockerfile -t blockchain-tests:${BRANCH} .
 
 echo -e "\033[33m Running tests... \033[0m"
-docker run -it blockchain-tests:${BRANCH} npm run test
+DOCKER_BUILDKIT=1 docker run -it blockchain-tests:${BRANCH} npm run test
 
 echo -e "\033[32m Done! \033[0m"
