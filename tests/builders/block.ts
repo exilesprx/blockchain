868d99c4f1161575a81b2c2983833cc028000b6a
Exilesprx <campbell.andrew86@yahoo.com>
2022-06-12 20:31:57 -0400

    WIP

---
+++ tests/builders/block.ts
@@ -0,0 +1,34 @@
+import { faker } from '@faker-js/faker';
+import { v4 } from 'uuid';
+import Block from '../../src/domain/chain/block';
+import Transaction from '../../src/domain/wallet/transaction';
+
+export default class BlockBuilder {
+  private block: Block;
+
+  public constructor() {
+    this.block = new Block(v4(), 0, 1, faker.unique.name, [], Date.now());
+  }
+
+  public withNoTransactions() : BlockBuilder {
+    this.block = new Block(v4(), 0, 1, faker.unique.name, [], Date.now());
+    return this;
+  }
+
+  public withOneTransaction() : BlockBuilder {
+    const transaction = new Transaction(
+      v4(),
+      faker.finance.litecoinAddress(),
+      faker.finance.litecoinAddress(),
+      Number(faker.finance.amount(0, 100)),
+      Date.now(),
+    );
+
+    this.block = new Block(v4(), 0, 1, faker.unique.name, [transaction], 0);
+    return this;
+  }
+
+  public build() : Block {
+    return this.block;
+  }
+}
