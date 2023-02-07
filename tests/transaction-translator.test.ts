4c0336fa52636ffb49b68618d9e7990a373495d0
Exilesprx <campbell.andrew86@yahoo.com>
2023-02-06 20:44:55 -0500

    Add tests

--- tests/transaction-translator.test.ts
+++ tests/transaction-translator.test.ts
@@ -1,35 +1,35 @@
 import TransactionTranslator from '../src/app/translators/transaction-translator';
 import Transaction from '../src/domain/wallet/transaction';
+import data from './stubs/transaction.json';
 
 describe('Transaction Translator', () => {
   test('it expects to translate an object into a Transaction', () => {
-    const message = {
-      id: 'one',
-      to: 'me',
-      from: 'you',
-      amount: 123,
-      date: Date.now(),
-      hash: 'abc123',
-    };
-
-    const transaction = TransactionTranslator.fromObject(message);
-    expect(transaction.getHash()).not.toBe('abc123');
+    const transaction = TransactionTranslator.fromObject(data);
+    expect(transaction.getHash()).not.toBe(data.hash);
     expect(transaction).toBeInstanceOf(Transaction);
   });
 
   test('it expects to translate a buffer into a transaction', () => {
-    const message = {
-      id: 'one',
-      to: 'me',
-      from: 'you',
-      amount: 123,
-      date: Date.now(),
-      hash: 'abc123',
-    };
-
-    const buffer = Buffer.from(JSON.stringify(message));
+    const buffer = Buffer.from(JSON.stringify(data));
     const transaction = TransactionTranslator.fromMessage(buffer);
-    expect(transaction.getAmount()).toBe(123);
-    expect(transaction.getHash()).not.toBe('abc123');
+    expect(transaction.getHash()).not.toBe(data.hash);
   });
+
+  test('it expects to translate an array of transactions', () => {
+    const transactions = [data, data];
+    const convertedTransactions = TransactionTranslator.fromObjectForMany(transactions);
+    convertedTransactions.forEach((transaction: Transaction) => {
+      expect(transaction.getHash()).not.toBe(data.hash);
+    });
+  });
+  
+  test('it expects transaction properties to match values parsed', () => {
+    const transaction = TransactionTranslator.fromObject(data);
+    expect(transaction).toBeInstanceOf(Transaction);
+    expect(transaction.getSender()).toBe(data.from);
+    expect(transaction.getReceiver()).toBe(data.to);
+    expect(transaction.getKey()).toBe(data.id);
+    expect(transaction.getDate()).toBe(data.date);
+    expect(transaction.getAmount()).toBe(data.amount);
+  })
 });
