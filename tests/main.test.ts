4592c68692ebc07eb17f1366b7bebe22642fc440
Exiles.prx <exiles.prx@gmail.com>
2025-02-21 19:53:17 -0500

    Use path mappings

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,17 +1,17 @@
 import { describe, expect, jest, test } from "@jest/globals";
-import Application from "../src/app/bank/main";
-import TransactionRoute from "../src/app/routes/transaction";
-import Server from "../src/app/server";
-import Database from "../src/infrastructure/database";
-import Blockchain from "../src/domain/chain/blockchain";
-import Link from "../src/domain/chain/specifications/link";
-import Producer from "../src/infrastructure/stream/producer";
-import Amount from "../src/domain/wallet/specifications/amount";
-import Receiver from "../src/domain/wallet/specifications/receiver";
-import SameWallet from "../src/domain/wallet/specifications/same-wallet";
-import Sender from "../src/domain/wallet/specifications/sender";
-import TransactionPool from "../src/domain/wallet/transaction-pool";
-import BlockMined from "../src/domain/chain/specifications/mined";
+import Application from "@/app/bank/main";
+import TransactionRoute from "@/app/routes/transaction";
+import Server from "@/app/server";
+import Database from "@/infrastructure/database";
+import Blockchain from "@/domain/chain/blockchain";
+import Link from "@/domain/chain/specifications/link";
+import Producer from "@/infrastructure/stream/producer";
+import Amount from "@/domain/wallet/specifications/amount";
+import Receiver from "@/domain/wallet/specifications/receiver";
+import SameWallet from "@/domain/wallet/specifications/same-wallet";
+import Sender from "@/domain/wallet/specifications/sender";
+import TransactionPool from "@/domain/wallet/transaction-pool";
+import BlockMined from "@/domain/chain/specifications/mined";
 
 // Abstract class are mocked differently then normal classes
 const emitter = {
@@ -21,13 +21,14 @@ const consumer = {
   run: jest.fn(),
   connect: jest.fn(),
 };
-jest.mock("../src/app/events/abstract-emitter", () => {
+jest.mock("@/app/events/abstract-emitter", () => {
   return {
     __esModule: true,
     default: jest.fn().mockImplementation(() => emitter),
   };
 });
-jest.mock("../src/infrastructure/stream/consumer", () => {
+
+jest.mock("@/infrastructure/stream/consumer", () => {
   return {
     __esModule: true,
     default: jest.fn().mockImplementation(() => consumer),
@@ -35,12 +36,12 @@ jest.mock("../src/infrastructure/stream/consumer", () => {
 });
 // End abstract class mocks
 
-jest.mock("../src/domain/wallet/transaction-pool");
-jest.mock("../src/domain/chain/blockchain");
-jest.mock("../src/infrastructure/database");
-jest.mock("../src/infrastructure/stream/producer");
-jest.mock("../src/app/routes/transaction");
-jest.mock("../src/app/server");
+jest.mock("@/domain/wallet/transaction-pool");
+jest.mock("@/domain/chain/blockchain");
+jest.mock("@/infrastructure/database");
+jest.mock("@/infrastructure/stream/producer");
+jest.mock("@/app/routes/transaction");
+jest.mock("@/app/server");
 
 describe("Main", () => {
   beforeAll(() => {
