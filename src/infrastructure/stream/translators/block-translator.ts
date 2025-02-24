11ec48ea522c67c27c9228c0a72c0767a04f3901
Exiles.prx <exiles.prx@gmail.com>
2025-02-23 23:47:35 -0500

    Destr for parsing json

--- src/infrastructure/stream/translators/block-translator.ts
+++ src/infrastructure/stream/translators/block-translator.ts
@@ -2,10 +2,11 @@ import TransactionTranslator from "../../../app/translators/transaction-translat
 import Block from "../../../domain/chain/block";
 import Transaction from "../../../domain/wallet/transaction";
 import { Block as BlockContract } from "../../database/models/block";
+import { destr } from "destr";
 
 export default class BlockTranslator {
   public static fromMessage(value: Buffer): Block {
-    const message: BlockContract = JSON.parse(value.toString());
+    const message: BlockContract = destr(value.toString());
     const transactions: Transaction[] = TransactionTranslator.fromObjectForMany(
       message.transactions,
     );
--- src/infrastructure/stream/translators/transaction-translator.ts
+++ src/infrastructure/stream/translators/transaction-translator.ts
@@ -1,9 +1,10 @@
 import Transaction from "../../../domain/wallet/transaction";
 import { Transaction as TransactionContract } from "../../database/models/transaction";
+import { destr } from "destr";
 
 export default class TransactionTranslator {
   public static fromMessage(value: Buffer): Transaction {
-    const message: TransactionContract = JSON.parse(value.toString());
+    const message: TransactionContract = destr(value.toString());
 
     return new Transaction(
       message.id,
