5f48c6cffaaba02fb2d06ba7c52619aba08641fb
Exiles.prx <exiles.prx@gmail.com>
2025-10-30 11:29:22 -0400

    fix: formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,75 +1,69 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+  allConfig: js.configs.all
 });
 
 export default [
   {
     ignores: [
-      "**/node_modules",
-      "**/jest.config.ts",
-      "build/**/*",
-      "coverage/**/*",
-      "eslint.config.mjs",
-    ],
+      '**/node_modules',
+      '**/jest.config.ts',
+      'build/**/*',
+      'coverage/**/*',
+      'eslint.config.mjs'
+    ]
   },
-  ...compat.extends("eslint:recommended", "plugin:prettier/recommended"),
+  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
   {
     plugins: {
-      "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint
     },
-
     languageOptions: {
       globals: {
-        ...globals.node,
+        ...globals.node
       },
-
       parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parserOptions: {
         project: true,
-        tsconfigRootDir: __dirname,
-      },
+        tsconfigRootDir: __dirname
+      }
     },
-
     settings: {
-      "import/resolver": {
+      'import/resolver': {
         node: {
-          extensions: [".js", ".ts"],
-        },
-      },
+          extensions: ['.js', '.ts']
+        }
+      }
     },
 
     rules: {
-      "no-console": 2,
-
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error"],
-    },
+      'no-console': 2,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error']
+    }
   },
   {
-    files: ["src/**/*/*.ts"],
+    files: ['src/**/*/*.ts']
   },
   {
-    files: ["tests/**/*.test.ts"],
-
+    files: ['tests/**/*.test.ts'],
     languageOptions: {
       globals: {
-        ...globals.jest,
-      },
-    },
-  },
+        ...globals.jest
+      }
+    }
+  }
 ];
